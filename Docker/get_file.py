98ac2ecdb635d7b687e4d4a934c720b88795d940
root <root@DESKTOP-8SMN2HO.>
2024-12-03 13:54:30 +0100

    un premier jet de docker file reste a faire le deploiment ansi que l'acces puis eventuellement un pipe de CI/CD

---
+++ Docker/get_file.py
@@ -0,0 +1,18 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
+
+# Remplacez par l'URL de votre compte de stockage
+account_url = "https://inputcontainerfornetflix.blob.core.windows.net"
+container_name = "test"
+
+# Authentification avec l'identité assignée
+credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
+
+# Récupérer le client du conteneur
+container_client = blob_service_client.get_container_client(container_name)
+
+# Lister les blobs dans le conteneur
+print("Liste des fichiers dans le conteneur :")
+for blob in container_client.list_blobs():
+    print(blob.name)
