1077d6504bb0d968d37cb9c9a254aee7253f41f7
salahait35 <136812449+salahait35@users.noreply.github.com>
2024-12-16 21:26:13 +0100

    finally done

--- Docker/workload/send_back_movie.py
+++ Docker/workload/send_back_movie.py
@@ -24,12 +24,12 @@ if check_status == "DONE":
 
 # Authentification avec l'identité assignée
     credential = DefaultAzureCredential(managed_identity_client_id=client_id)
-    blob_service_client_read = BlobServiceClient(account_url=f"https://{os.getenv('AZURE_BLOB_READ')}.blob.core.windows.net", credential=credential)
-    blob_service_client_write = BlobServiceClient(account_url=f"https://{os.getenv('AZURE_BLOB_WRITE')}.blob.core.windows.net", credential=credential)
+    blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=credential)
+    
 
     # Récupérer le client du conteneur et du blob pour lecture et écriture
-    container_client_read = blob_service_client_read.get_container_client(container_name)
-    container_client_write = blob_service_client_write.get_container_client(container_name)
+    container_client_read = blob_service_client.get_container_client(os.getenv('AZURE_BLOB_READ'))
+    container_client_write = blob_service_client.get_container_client(os.getenv('AZURE_BLOB_WRITE'))
 
     original_blob_client = container_client_read.get_blob_client(blob_name)
     converted_blob_client = container_client_write.get_blob_client(f"{os.path.splitext(blob_name)[0]}.mp4")
