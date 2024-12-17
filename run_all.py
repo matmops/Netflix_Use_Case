0c65dcbdcac0eb5cb257684c48cf6fb8e96654cc
Mathéo Labban <75206019+matmops@users.noreply.github.com>
2024-12-17 12:27:11 +0100

    Update Dockerfile to use run_all.py as the default command and add run_all.py script

---
+++ run_all.py
@@ -0,0 +1,24 @@
+import subprocess
+import time
+
+def run_script(script_name):
+    try:
+        subprocess.run(["python", script_name], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while running {script_name}: {e}")
+
+if __name__ == "__main__":
+    scripts = [
+        "get_files.py",
+        "get_movie.py",
+        "process_movie.py",
+        "check_movie.py",
+        "send_back_movie.py"
+    ]
+
+    for script in scripts:
+        run_script(script)
+
+    # Keep the container running indefinitely
+    while True:
+        time.sleep(3600)
